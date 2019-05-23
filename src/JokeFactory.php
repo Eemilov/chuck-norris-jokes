7b65cb17beaf672b8cd08954e79c2fe78eef298b
Emil Emilov <eemilovw@gmail.com>
2019-05-23 16:23:02 +0000

    Apply fixes from StyleCI

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -7,15 +7,16 @@ class JokeFactory
     protected $jokes = [
         'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
         'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
-        'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.'
+        'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
     ];
 
     public function __construct(array $jokes = null)
     {
-        if (!empty($jokes)) {
+        if (! empty($jokes)) {
             $this->jokes = $jokes;
         }
     }
+
     public function getRandomJoke()
     {
         return $this->jokes[array_rand($this->jokes)];
